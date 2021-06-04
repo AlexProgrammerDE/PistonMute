e85ba8a2bb4297fdfac366b136411abfa4707bb9
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-06-04 20:58:30 +0200

    Improve logging and add bstats

--- src/main/java/net/pistonmaster/pistonmute/PistonMute.java
+++ src/main/java/net/pistonmaster/pistonmute/PistonMute.java
@@ -4,6 +4,7 @@ import net.pistonmaster.pistonmute.commands.MuteCommand;
 import net.pistonmaster.pistonmute.commands.UnMuteCommand;
 import net.pistonmaster.pistonmute.listeners.PistonChatListener;
 import net.pistonmaster.pistonmute.utils.StorageTool;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,14 +18,19 @@ public final class PistonMute extends JavaPlugin {
         log.info(ChatColor.YELLOW + "Loading config");
         StorageTool.setupTool(this);
 
-        log.info(ChatColor.YELLOW + "Registering command");
+        log.info(ChatColor.YELLOW + "Registering commands");
         getServer().getPluginCommand("mute").setExecutor(new MuteCommand(this));
         getServer().getPluginCommand("mute").setTabCompleter(new MuteCommand(this));
 
         getServer().getPluginCommand("unmute").setExecutor(new UnMuteCommand(this));
         getServer().getPluginCommand("unmute").setTabCompleter(new UnMuteCommand(this));
 
-        log.info(ChatColor.YELLOW + "Registering chat listener");
+        log.info(ChatColor.YELLOW + "Registering listeners");
         getServer().getPluginManager().registerEvents(new PistonChatListener(), this);
+
+        getLogger().info(ChatColor.YELLOW + "Loading metrics");
+        new Metrics(this, 11559);
+
+        getLogger().info(ChatColor.YELLOW + "Done! :D");
     }
 }
