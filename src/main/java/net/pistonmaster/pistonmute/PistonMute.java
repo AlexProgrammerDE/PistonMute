760d83bc169a5f231fbb33493f63d5ed636508f4
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-06-05 12:16:29 +0200

    Add update checker

--- src/main/java/net/pistonmaster/pistonmute/PistonMute.java
+++ src/main/java/net/pistonmaster/pistonmute/PistonMute.java
@@ -1,11 +1,15 @@
 package net.pistonmaster.pistonmute;
 
+import net.md_5.bungee.api.ChatColor;
 import net.pistonmaster.pistonmute.commands.MuteCommand;
 import net.pistonmaster.pistonmute.commands.UnMuteCommand;
 import net.pistonmaster.pistonmute.listeners.PistonChatListener;
 import net.pistonmaster.pistonmute.utils.StorageTool;
+import net.pistonmaster.pistonutils.logging.PistonLogger;
+import net.pistonmaster.pistonutils.update.UpdateChecker;
+import net.pistonmaster.pistonutils.update.UpdateParser;
+import net.pistonmaster.pistonutils.update.UpdateType;
 import org.bstats.bukkit.Metrics;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -28,8 +32,26 @@ public final class PistonMute extends JavaPlugin {
         log.info(ChatColor.YELLOW + "Registering listeners");
         getServer().getPluginManager().registerEvents(new PistonChatListener(), this);
 
-        getLogger().info(ChatColor.YELLOW + "Loading metrics");
+        log.info(ChatColor.YELLOW + "Loading metrics");
         new Metrics(this, 11559);
+
+        log.info(ChatColor.YELLOW + "Checking for a newer version");
+        new UpdateChecker(new PistonLogger(getLogger())).getVersion("https://www.pistonmaster.net/PistonMute/VERSION.txt", version -> new UpdateParser(getDescription().getVersion(), version).parseUpdate(updateType -> {
+            if (updateType == UpdateType.NONE || updateType == UpdateType.AHEAD) {
+                log.info(ChatColor.DARK_GREEN + "You're up to date!");
+            } else {
+                if (updateType == UpdateType.MAJOR) {
+                    log.info(ChatColor.RED + "There is a MAJOR update available!");
+                } else if (updateType == UpdateType.MINOR) {
+                    log.info(ChatColor.RED + "There is a MINOR update available!");
+                } else if (updateType == UpdateType.PATCH) {
+                    log.info(ChatColor.RED + "There is a PATCH update available!");
+                }
+
+                log.info(ChatColor.RED + "Current version: " + this.getDescription().getVersion() + " New version: " + version);
+                log.info(ChatColor.RED + "Download it at: https://github.com/AlexProgrammerDE/PistonMute/releases");
+            }
+        }));
 
         getLogger().info(ChatColor.YELLOW + "Done! :D");
     }
